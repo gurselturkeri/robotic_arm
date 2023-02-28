2d75bfc1ec04e13b89f65789c79c1c3690bb1990
Gursel <gurselt33@gmail.com>
2023-02-28 19:10:09 +0300

    controller changed

--- install/robotic_arm/share/robotic_arm/launch/controller.launch.py
+++ install/robotic_arm/share/robotic_arm/launch/controller.launch.py
@@ -1,85 +1,48 @@
-
+import os
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
-from launch.event_handlers import OnProcessExit
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch.actions import ExecuteProcess
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
-
+from scripts import GazeboRosPaths
 
 def generate_launch_description():
-
-    robot_description_content = Command(
+    package_share_dir = get_package_share_directory("robotic_arm")    # Add package name
+    urdf_file = os.path.join(package_share_dir, "urdf", "robotic_arm.urdf")  # Add urdf name
+    controller_file = os.path.join(package_share_dir, "config", "jtc.yaml")
+    robot_description = {"robot_description": urdf_file}
+    return LaunchDescription(
         [
-            PathJoinSubstitution([FindExecutable(name='xacro')]),
-            ' ',
-            PathJoinSubstitution(
-                [FindPackageShare(
-                    'robotic_arm'),
-                    'urdf',
-                    'robotic_arm.urdf']
+            ExecuteProcess(
+                cmd=["gazebo","-s","libgazebo_ros_factory.so",],
+                output="screen",
             ),
+            Node(
+                package="gazebo_ros",
+                executable="spawn_entity.py",
+                arguments=["-entity","robotic_arm","-b","-file", urdf_file,],    # Add package name
+            ),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                arguments=[urdf_file],
+            ),
+
+            Node(
+                package="controller_manager",
+                executable="ros2_control_node",
+                parameters=[robot_description, controller_file],
+                output="screen",
+            ),
+            Node(
+            	package="controller_manager",
+            	executable="spawner",
+            	arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+            ),
+            Node(
+                package="controller_manager",
+                executable="spawner",
+                arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
+            )
         ]
     )
-    robot_description = {'robot_description': robot_description_content}
-
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[robot_description]
-    )
-
-    camera_node = Node(
-        package='camera_ros',
-        executable='camera_ros',
-        name='camera_ros',
-        output='screen',
-  
-    )
-
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [FindPackageShare('gazebo_ros'), '/launch', '/gazebo.launch.py']
-        ),
-    )
-
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'joints'],
-                        output='screen')
-
-    load_joint_state_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    load_trajectory_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_trajectory_controller'],
-        output='screen'
-    )
-    
-   
-
-    return LaunchDescription([
-        RegisterEventHandler(
-          event_handler=OnProcessExit(
-                target_action=spawn_entity,
-                on_exit=[load_joint_state_controller],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=load_joint_state_controller,
-                on_exit=[load_trajectory_controller],
-            )
-        ),
-    
-        gazebo,
-        camera_node,
-        node_robot_state_publisher,
-        spawn_entity,
-    ])
